c472496ed57e795f9e77bed61eb4dd8a96ab1e17
RubyRed19 <mariaquintanamsis@gmail.com>
2022-09-06 08:44:12 -0700

    Update.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,14 +1,32 @@
 public class Television {
+  private String brand;
+  private int volume;
 
-  String brand;
-  int volume;
-
-    void turnOn() {
+    public void turnOn() {
       System.out.println("The " + brand + " television is on, with a volume of " + volume);
     }
 
-    void turnOff() {
+    public void turnOff() {
       System.out.println("The " + brand + " television is off");
     }
 
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  public String toString(){
+      return "Television: brand = " + brand + ", volume = " + volume;
+  }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,18 +3,21 @@ public class TelevisionClient {
   public static void main(String[] args) {
 
     Television tv1 = new Television();
-    tv1.brand = "Samsung";
-    tv1.volume = 32;
+    tv1.setBrand("Samsung");
+    tv1.setVolume(32);
 
 
     Television tv2 = new Television();
-    tv2.brand = "Sony";
-    tv2.volume = 50;
+    tv2.setBrand("Sony");
+    tv2.setVolume(50);
 
     tv1.turnOn();
     tv1.turnOff();
     tv2.turnOn();
     tv2.turnOff();
+
+    System.out.println(tv1.toString());
+    System.out.println(tv1);
   }
 
 }
